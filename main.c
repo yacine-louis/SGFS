9361b2c5db54d07cfe09e17e828c3d4705b4dde9
Louis <87730062+yacine-louis@users.noreply.github.com>
2024-12-09 21:27:36 +0100

    Implement the initFileSystem function

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 // define structures
 typedef struct Block Block;
 typedef struct Meta Meta;
-typedef struct Virtualdisk Virtualdisk;
+typedef struct MS MS;
 
 struct Block {
      char data[256];
@@ -13,7 +13,7 @@ struct Block {
 };
 
 struct Meta {
-     char name[16];
+     char name[12];
      int startBlock; // First block of the file
      int FileSizeInBlocks; // File size in blocks
      int FileSizeInRecords; // File size in records
@@ -21,14 +21,53 @@ struct Meta {
      int internalOrganisationMode; // 0 for records not sorted, 1 for records sorted, -1 if user havn't choosen yet
 };
 
-struct Virtualdisk {
+struct MS {
      int numberOfBlocks;
      int blockSize;
      int numberOfMeta;
      Block* disk;
 };
 
+// Functions associated with virtual disk
+Meta* inodes;
+MS db; 
 
+void initFileSystem() {
+     // initialize informations about the file system;
+     db.numberOfBlocks = 256;
+     db.blockSize = sizeof(Block) - sizeof(int);
+     db.numberOfMeta = 20;
+
+     // initialize metadata
+     inodes = malloc(db.numberOfMeta * sizeof(Meta));
+     for (int i = 0; i < db.numberOfMeta; i++)
+     {
+          srtcpy(inodes[i].name, "");
+          inodes[i].startBlock = -1;
+          inodes[i].FileSizeInBlocks = 0;
+          inodes[i].FileSizeInRecords = 0;
+          inodes[i].globalOrganisationMode = -1;
+          inodes[i].internalOrganisationMode = -1;
+     }
+
+     // initialize MS
+     db.disk = malloc(db.numberOfBlocks * sizeof(Block));
+     // initialize the allocation table
+     for (int i = 0; i < db.blockSize; i++)
+     {
+          db.disk->data[i] = 0;
+     }
+
+     // initialize the disk
+     for (int i = 0; i < db.numberOfBlocks; i++)
+     {
+          for (int j = 0; j < db.blockSize; j++)
+          {
+               db.disk->data[i] = 0;
+          }
+     }
+     
+}
 
 int main() {
 
